526b9498e79777c21c37e1b4d18fb7616ef8c2dd
JorgeCal123/boom.git <git@github.com>
2022-05-30 01:54:10 +0000

    task 13

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,6 +3,7 @@
 Module app
 """
 from flask import Flask, Blueprint, jsonify
+from flask_cors import CORS
 from models import storage
 from os import getenv
 from api.v1.views import app_views
@@ -11,6 +12,7 @@ from api.v1.views import app_views
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def teardown(exception):
@@ -21,6 +23,7 @@ def teardown(exception):
 @app.errorhandler(404)
 def error(exception):
     return jsonify({'error': 'Not found'}), 404
+
 
 
 if __name__ == "__main__":
