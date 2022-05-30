194bd8fe2da75979783ea7d66c025bf22be206b5
JorgeCal123/boom.git <git@github.com>
2022-05-30 03:26:41 +0000

    correction file app

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,9 +1,9 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 """
 Module app
 """
 from flask import Flask, Blueprint, jsonify
-#from flask_cors import CORS
+from flask_cors import CORS
 from models import storage
 from os import getenv
 from api.v1.views import app_views
@@ -12,7 +12,8 @@ from api.v1.views import app_views
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
-#cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
+
 
 @app.teardown_appcontext
 def teardown(exception):
@@ -23,7 +24,6 @@ def teardown(exception):
 @app.errorhandler(404)
 def error(exception):
     return jsonify({'error': 'Not found'}), 404
-
 
 
 if __name__ == "__main__":
