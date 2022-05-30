0845f4439c0f35070087ec46023a6716d4e96d21
JorgeCal123 <43484686+JorgeCal123@users.noreply.github.com>
2022-05-29 21:29:59 -0500

    Update db_storage.py
    change method get

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -79,7 +79,9 @@ class DBStorage:
         """Returns the object based on the class and its
         ID, or None if not found"""
         dictt = self.all(cls)
-        key = cls + "." + id
+        for key, value in classes.items():
+            if cls == value:
+                cls = key
         if key in dictt:
             return(dictt[key])
         return(None)
