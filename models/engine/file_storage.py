35293d17c1067472f5ddcb274a557cd1d070d891
JorgeCal123 <43484686+JorgeCal123@users.noreply.github.com>
2022-05-29 21:30:40 -0500

    Update file_storage.py
    change method get in filestorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -73,7 +73,9 @@ class FileStorage:
         """Returns the object based on the class and its
         ID, or None if not found"""
         dictt = self.all(cls)
-        key = cls + "." + id
+        for key, value in classes.items():
+            if cls == value:
+                cls = key
         if key in dictt:
             return(dictt[key])
         return(None)
